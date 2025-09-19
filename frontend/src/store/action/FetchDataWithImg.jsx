311ba739441056895340e74010ca4ab8eb53c616
Bhautik Sangani <161152146+bhautik124@users.noreply.github.com>
2025-09-19 12:28:01 +0530

    Update FetchDataWithImg.jsx

--- frontend/src/store/action/FetchDataWithImg.jsx
+++ frontend/src/store/action/FetchDataWithImg.jsx
@@ -4,7 +4,7 @@ import { fetchData } from "../reducer/GetAllDataOfModelWithImgReducer";
 export const fetchDataWithImg = () => async (dispatch) => {
   try {
     const response = await axios.post(
-      "http://localhost:8000/api/generate-key",
+      "https://backendlibrary-fullstack-backend.onrender.com/api/generate-key",
       { feature: "crud-with-image-get-all-model-with-data" },
       { withCredentials: true }
     );
