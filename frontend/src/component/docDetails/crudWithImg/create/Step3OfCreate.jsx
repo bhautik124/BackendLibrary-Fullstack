73f51a125698437170fbe201aeaa6058782171c5
Bhautik Sangani <161152146+bhautik124@users.noreply.github.com>
2025-09-19 12:14:06 +0530

    Update Step3OfCreate.jsx

--- frontend/src/component/docDetails/crudWithImg/create/Step3OfCreate.jsx
+++ frontend/src/component/docDetails/crudWithImg/create/Step3OfCreate.jsx
@@ -69,7 +69,7 @@ const Step3OfCreate = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:8000/api/generate-key",
+        "https://backendlibrary-fullstack-backend.onrender.com/api/generate-key",
         { feature: "crud-with-image-create-model" },
         { withCredentials: true }
       );
@@ -107,7 +107,7 @@ const Step3OfCreate = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:8000/api/generate-key",
+        "https://backendlibrary-fullstack-backend.onrender.com/api/generate-key",
         { feature: "crud-with-image-create-model" },
         { withCredentials: true }
       );
